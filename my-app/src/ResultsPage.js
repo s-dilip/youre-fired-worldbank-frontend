3c7b6cb8ad60ad19935ca5efe941ea152c9335d5
Swastik Dilip <justsyd224@gmail.com>
2022-12-22 17:14:19 +0000

    Graph presentation works (Hard Coded)

--- my-app/src/ResultsPage.js
+++ my-app/src/ResultsPage.js
@@ -1,10 +1,46 @@
+import { useState } from "react";
 import IndicatorChart from "./components/IndicatorChart";
 
 export default function ResultsPage() {
+  const country = "Germany";
+  const startYear = 1995;
+  const endYear = 2011;
+  const indicator = "CO2 emissions (kt)";
+
+  let years = [];
+
+  const [dataPoints, setDataPoints] = useState([]);
+
+  for (let i = startYear; i < endYear + 1; i++) {
+    years.push(i);
+  }
+
+  async function fetchIndicatorForYear(year) {
+    const apiResponse = await fetch(
+      `http://127.0.0.1:5000/countries/${country}/${indicator}/${year}`
+    );
+    const dataPoint = await apiResponse.json();
+    return dataPoint[0].value;
+  }
+
+  (async function () {
+    let indicatorsData = [];
+    for (const year of years) {
+      const datapoint = await fetchIndicatorForYear(year);
+      indicatorsData.push(datapoint);
+    }
+    setDataPoints(indicatorsData);
+  })();
+
   return (
     <div>
       <h1>results</h1>
-      <IndicatorChart />
+      <IndicatorChart
+        datapoints={dataPoints}
+        years={years}
+        countryName={country}
+        indicatorChosen={indicator}
+      />
     </div>
   );
 }
--- my-app/src/components/IndicatorChart.js
+++ my-app/src/components/IndicatorChart.js
@@ -1,18 +1,24 @@
 import Plot from "react-plotly.js";
 
-export default function IndicatorChart() {
+export default function IndicatorChart(props) {
   return (
     <div>
       <Plot
         data={[
           {
             type: "scatter",
-            x: ["One", "Two", "Three"],
-            y: [1, 2, 3],
+            x: props.years,
+            y: props.datapoints,
           },
         ]}
-        layout={{ width: 700, height: 500, title: "A Fancy Plot" }}
+        layout={{
+          width: 700,
+          height: 500,
+          title: `${props.countryName}'s ${props.indicatorChosen}`,
+        }}
       />
+      <h2>{props.datapoints}</h2>
+      <h2>{props.years}</h2>
     </div>
   );
 }
